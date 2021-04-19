9580277a82cfd0fce2dd8ffb574c1ce5d398d7eb
cr-velox-git <khangame78@gmail.com>
2021-04-19 13:41:42 +0530

    create chatroom

--- components/ContactsListItem/index.tsx
+++ components/ContactsListItem/index.tsx
@@ -5,6 +5,9 @@ import styles from './style';
 
 import { useNavigation } from '@react-navigation/native';
 
+import { API, graphqlOperation, Auth } from 'aws-amplify';
+import { createChatRoom, createChatRoomUser } from '../../src/graphql/mutations';
+
 export type ChatListItemsProps = {
     user: Users;
 }
@@ -14,22 +17,72 @@ const ContactListItems = (props: ChatListItemsProps) => {
 
     const navigation = useNavigation();
 
-    const onCLick = () => {
-        // navigate to chat room with this user
+    const onCLick = async () => {
+        try {
+
+            // 1. create a new chat room
+            const newChatRoomData = await API.graphql(
+                graphqlOperation(
+                    createChatRoom, { input: {} }
+                )
+            )
+
+            if (!newChatRoomData.data) {
+                console.log("fail to create chat room")
+                return;
+            }
+
+            const newChatRoom = newChatRoomData.data.createChatRoom;
+
+            // 2. And 'user' to the Chat Room
+            await API.graphql(
+                graphqlOperation(
+                    createChatRoomUser, {
+                    inpute: {
+                        userID: user.id,
+                        chatRoomID: newChatRoom.id,
+                    } 
+                }
+                )
+            )
+
+            //3. Add authentication user to the Chat room
+            const userInfo = await Auth.currentAuthenticatedUser();
+            await API.graphql(
+                graphqlOperation(
+                    createChatRoomUser, {
+                    input: {
+                        userID: userInfo.attribute.sub,
+                        chatRoomID: newChatRoom.id,
+                    }
+                }
+                )
+            )
+
+            navigation.navigate('ChatRoomScreen', {
+                id: newChatRoom.id,
+                name: user.name,
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return (
-        <TouchableWithoutFeedback onPress = {onCLick}>
-            <View style={styles.container}>
+
+        <View style={styles.container}>
+            <TouchableWithoutFeedback onPress={onCLick}>
                 <View style={styles.lefContainer}>
-                    <Image source={{uri: user.imageUri }} style={styles.avatar} />
+                    <Image source={{ uri: user.imageUri }} style={styles.avatar} />
                     <View style={styles.midContainer}>
                         <Text style={styles.userName}>{user.name}</Text>
                         <Text style={styles.status}>{user.status}</Text>
                     </View>
                 </View>
-            </View>
-        </TouchableWithoutFeedback>
+            </TouchableWithoutFeedback>
+        </View>
+
     )
 };
 
--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -4,15 +4,37 @@ import { Text, View } from '../components/Themed';
 import Users from '../data/Users';
 import { FlatList } from 'react-native-gesture-handler';
 import ContactListItems from '../components/ContactsListItem';
+import { graphqlOperation } from '@aws-amplify/api-graphql';
 
+import { listUsers } from '../src/graphql/queries';
+import { useEffect } from 'react';
+import { API } from 'aws-amplify';
+import { useState } from 'react';
 
 export default function ChatScreen() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const usersData = await API.graphql(
+          graphqlOperation(listUsers)
+        )
+        setUsers(usersData.data.listUsers.items);
+        console.log(usersData)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
       <FlatList
-      style={{width: '100%'}}
+        style={{ width: '100%' }}
         data={Users}
-        renderItem={({item})=><ContactListItems user={item}/>}
+        renderItem={({ item }) => <ContactListItems user={item} />}
         keyExtractor={(item) => item.id}
       />
     </View>
